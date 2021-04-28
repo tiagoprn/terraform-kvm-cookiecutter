d8856c31fb5ec9265c251365bf964810b28f7098
tiago.paranhos <tiagoprn@gmail.com>
2021-04-28 07:03:16 -0300

    feat: removed size option, not supported with source option.

--- {{ cookiecutter.vm_name }}/libvirt-kvm.tf
+++ {{ cookiecutter.vm_name }}/libvirt-kvm.tf
@@ -13,7 +13,6 @@ resource "libvirt_volume" "{{ cookiecutter.vm_name }}-qcow2" {
   name   = "{{ cookiecutter.vm_name }}-qcow2"
   pool   = libvirt_pool.{{ cookiecutter.vm_name }}.name
   source = "{{ cookiecutter.volume_source }}"
-  size = "{{ cookiecutter.volume_size }}"
   format = "qcow2"
 }
 
